aadee8adabf2ec12ea8873663a2416c7ded8814f
Lixtron <100446393+Lixtron@users.noreply.github.com>
2026-07-07 01:30:42 +0100

    Create squadronunitpity.lua

---
+++ squadronunitpity.lua
@@ -0,0 +1,41 @@
+return {
+    ["baras"] = {
+        Name = "Baras",
+        Limit = 100,
+    },
+
+    ["berserker"] = {
+        Name = "Berserker",
+        Limit = 300,
+    },
+
+    ["bralu"] = {
+        Name = "Bralu",
+        Limit = 50,
+    },
+
+    ["garu"] = {
+        Name = "Garu",
+        Limit = 100,
+    },
+
+    ["madora"] = {
+        Name = "Madora",
+        Limit = 300,
+    },
+
+    ["puppeteer"] = {
+        Name = "Puppeteer",
+        Limit = 100,
+    },
+
+    ["shanron"] = {
+        Name = "Shanron",
+        Limit = 400,
+    },
+
+    ["skeleton_knight"] = {
+        Name = "Skeleton Knight",
+        Limit = 100,
+    },
+}
