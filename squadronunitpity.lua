c6a20622096d78052944143f5c811f1f4711456e
Lixtron <100446393+Lixtron@users.noreply.github.com>
2026-07-07 01:54:57 +0100

    Update squadronunitpity.lua

--- squadronunitpity.lua
+++ squadronunitpity.lua
@@ -7,11 +7,32 @@ return {
     ["berserker"] = {
         Name = "Berserker",
         Limit = 300,
+        Difficulties = {
+            Normal = {
+                Limit = 300,
+            },
+            Hard = {
+                Limit = 200,
+            },
+        },
     },
 
     ["bralu"] = {
         Name = "Bralu",
         Limit = 50,
+    },
+
+    ["freezer"] = {
+        Name = "Freezer",
+        Limit = 200,
+        Difficulties = {
+            Normal = {
+                Limit = 200,
+            },
+            Hard = {
+                Limit = 100,
+            },
+        },
     },
 
     ["garu"] = {
@@ -22,6 +43,14 @@ return {
     ["madora"] = {
         Name = "Madora",
         Limit = 300,
+        Difficulties = {
+            Normal = {
+                Limit = 300,
+            },
+            Hard = {
+                Limit = 200,
+            },
+        },
     },
 
     ["puppeteer"] = {
@@ -29,18 +58,29 @@ return {
         Limit = 100,
     },
 
-    ["freezer"] = {
-        Name = "Freezer",
-        Limit = 100,
-    },
-
     ["shanron"] = {
         Name = "Shanron",
         Limit = 400,
+                Difficulties = {
+            Normal = {
+                Limit = 400,
+            },
+            Hard = {
+                Limit = 200,
+            },
+        },
     },
 
     ["skeleton_knight"] = {
         Name = "Skeleton Knight",
-        Limit = 100,
+        Limit = 400,
+                Difficulties = {
+            Normal = {
+                Limit = 400,
+            },
+            Hard = {
+                Limit = 200,
+            },
+        },
     },
 }
