c5bfa01c068906748af4d96f02e597b6a97c371d
Lixtron <100446393+Lixtron@users.noreply.github.com>
2026-07-07 01:31:19 +0100

    Create squadronrrlimits.lua

---
+++ squadronrrlimits.lua
@@ -0,0 +1,26 @@
+return {
+    ["challenge_katakara_bridge_1_trait_shards"] = {
+        Name = "Katakara Bridge",
+        Limit = 100,
+    },
+
+    ["challenge_the_hero_hunter_1_trait_shards"] = {
+        Name = "The Hero Hunter",
+        Limit = 30,
+    },
+
+    ["raid_eclipse_(before)_4_trait_shards"] = {
+        Name = "Eclipse (Before)",
+        Limit = 100,
+    },
+
+    ["raid_gt_city_4_trait_shards"] = {
+        Name = "GT City",
+        Limit = 100,
+    },
+
+    ["invasion_the_lava_continent_4_trait_shards"] = {
+        Name = "The Lava Continent",
+        Limit = 40,
+    },
+}
